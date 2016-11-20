b4322b3b6e07549418378eb1940e6a5bda0dcf97
vasu <vasujammula@outlook.com>
2016-11-20 11:14:08 +0530

    This example illustrates operators and variables in python

---
+++ operators/operator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#i=10
+#j=20
+#k=i+j
+v1=input("Please enter value1:")
+v2=input("Please enter value2:")
+a=v1+v2
+s=v1-v2
+print "SO YOU ENTERED %s and %s"%(v1,v2)
+print "v1 is of type"+str(type(v1))+"v2 is of type"+str(type(v2))
+print "SO YOU ENTERED"+str(v1)+"and"+str(v2)
+print "ADDITION OF v1 and v2 is :",a
+print "SUB of v1 and v2 is:",s
+print "MUL of v1 and v2",v1*v2
+print "POWER of v1 and v2",v1**v2
